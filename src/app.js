76956cb7ab7609d2860b7ae592618cb91591c485
Camilo Córdoba <camilocordobaa@gmail.com>
2024-06-03 12:55:38 -0300

    configuracion handlebars y socket

--- src/app.js
+++ src/app.js
@@ -1,13 +1,27 @@
 import express from "express";
 import routes from "./routes/index.js";
+import __dirname from "./dirname.js"
+import handlebars  from "express-handlebars";
+import { Server } from "socket.io";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(express.static("public"));
 
 app.use("/api", routes);
 
-app.listen(8080, ()=> {
+const httpServer = app.listen(8080, ()=> {
     console.log("Servidor escuchando en el puerto 8080")
+});
+
+// socket io
+
+const io = new Server(httpServer);
+io.on("connection", (socket) => {
+    console.log("Nuevo usuario conectado");
 });
